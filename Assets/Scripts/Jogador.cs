ec680c16fec2af019ec4bfbf8df2954146fbafb0
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2017-09-26 15:01:22 -0300

    E o jogo continua..
    Sprite do personagem gira

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -7,11 +7,13 @@ public class Jogador : MonoBehaviour {
 	public Rigidbody2D rb2d;
 	public bool podePular;
 	public float velocidade = 5f;
+	private SpriteRenderer sR;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
 		podePular = false;
+		sR = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +21,12 @@ public class Jogador : MonoBehaviour {
 		float andar = Input.GetAxis("Horizontal") * velocidade;
 		this.transform.position += new Vector3(andar, 0) * Time.deltaTime;
 		if (podePular == true && Input.GetKeyDown (KeyCode.Space)) {
-			rb2d.AddForce (Vector2.up * 3, ForceMode2D.Impulse);
+			rb2d.AddForce (Vector2.up * 2, ForceMode2D.Impulse);
+		}
+		bool flipSprite = (sR.flipX ? (andar > 0.01f) : (andar < 0.01f));
+		if (flipSprite) 
+		{
+			sR.flipX = !sR.flipX;
 		}
 	}
 
