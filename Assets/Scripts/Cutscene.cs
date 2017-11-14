2ded6fff274aa010b0b3890f00294f7cb890e035
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2017-11-14 12:15:44 -0200

    Update cutscene
    Adicionado script para cutscene

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class Cutscene : MonoBehaviour {
 
-	public GameObject cutsceneCamera;
-	Vector3 local;
-	Vector3 ir;
+	public Transform inicio;
+	public Transform final;
+	public float velocidade = 1.0F;
+	private float comecarTempo;
+	private float tamanhoCaminho;
+
+	private bool chegouPorta;
+	bool voltar = false;
+
+	private IEnumerator coroutine;
 
 	// Use this for initialization
 	void Start () {
-		local = cutsceneCamera.transform.position;
+		comecarTempo = Time.time;
+		tamanhoCaminho = Vector3.Distance(inicio.position, final.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Jogador.abrirPorta) {
-			//Time.timeScale = 0;
-			//ir.Lerp()
+			float distCovered = (Time.time - comecarTempo) * velocidade;
+			float fracJourney = distCovered / tamanhoCaminho;
+			transform.position = Vector3.Lerp(inicio.position, final.position, fracJourney);
+			coroutine = tempoAbrirPorta();
+			StartCoroutine(coroutine);
+			if (voltar == true) {
+				transform.position = Vector3.Lerp(final.position, inicio.position, fracJourney);
+				Jogador.abrirPorta = false;
+			}
+		}
+	}
+
+	private IEnumerator tempoAbrirPorta(){
+		while (chegouPorta) {
+			yield return new WaitForSeconds (5f);
+			chegouPorta = false;
+			voltar = true;
 		}
 	}
 }
