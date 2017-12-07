50a11779702bbfd65814ba6668283f2150e14b85
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2017-12-07 12:32:35 -0200

    Update
    Adicionado o puzzle da biblioteca e script para facilitar inputs

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class Jogador : MonoBehaviour {
 
+	public MovimentoPlayer Mp;
+
 	public int health = 2;
 
 	public Rigidbody2D rb2d;
@@ -14,12 +16,27 @@ public class Jogador : MonoBehaviour {
 	private SpriteRenderer sR;
 	public bool irFaseDois = false;
 
+	public GameObject Puzzle;
+
 	// 0 para rua, 1 pra biblioteca...
 	static public int fase = 0;
 
 	public GameObject botao;
 	public GameObject livro;
 	public GameObject interagivel;
+	public bool PegueiLivro = false;
+
+	public GameObject botaoCO;
+	public GameObject CaixaO;
+	public GameObject interagivelCaixaO;
+
+	public GameObject botaoCP;
+	public GameObject CaixaP;
+	public GameObject interagivelCaixaP;
+
+	public GameObject botaoCB;
+	public GameObject CaixaB;
+	public GameObject interagivelCaixaB;
 
 	public GameObject botaoSubir;
 	public GameObject interagivelSubir;
@@ -33,6 +50,8 @@ public class Jogador : MonoBehaviour {
 	static public bool proximaCena = false;
 
 	public bool mudarAndar;
+
+	public bool HabilitarPuzzle = false;
 
 	public void TomaDano( int dano )
 	{
@@ -66,13 +85,23 @@ public class Jogador : MonoBehaviour {
 
 		float andar = Input.GetAxis("Horizontal") * velocidade;
 		this.transform.position += new Vector3(andar, 0) * Time.deltaTime;
-		if (podePular == true && Input.GetKeyDown (KeyCode.Space)) {
+
+		if (podePular == true && Input.GetKeyDown (Mp.jumpKey)) {
 			rb2d.AddForce (Vector2.up * 4, ForceMode2D.Impulse);
 		}
 		bool flipSprite = (sR.flipX ? (andar > 0.01f) : (andar < 0.01f));
 		if (flipSprite) 
 		{
 			sR.flipX = !sR.flipX;
+		}
+		if (HabilitarPuzzle & Input.GetKey (KeyCode.I)) {
+			if (Puzzle.activeSelf) {
+				Puzzle.SetActive (false);
+			}
+			else
+			{
+				Puzzle.SetActive (true);
+			}
 		}
 
 		if (irFaseDois == true & Input.GetKey(KeyCode.W) || irFaseDois == true & Input.GetKey(KeyCode.UpArrow)) {
@@ -87,10 +116,24 @@ public class Jogador : MonoBehaviour {
 			MovimentoCamera.floor = 0;
 			this.transform.position = new Vector2(10.6f, -1.0487f);
 		}
-		if (botao.activeSelf & Input.GetKey (KeyCode.E)) {
+		if (botao.activeSelf & Input.GetKey (Mp.InteractKey)) {
 			livro.SetActive(false);
 			interagivel.SetActive (false);
+			HabilitarPuzzle = true;
+			PegueiLivro = true;
+		}
+		if (botaoCO.activeSelf & Input.GetKey (Mp.InteractKey) & PegueiLivro) {
+			botaoCO.SetActive(false);
+			interagivelCaixaO.SetActive (false);
+		}
+		if (botaoCP.activeSelf & Input.GetKey (Mp.InteractKey) & PegueiLivro) {
+			botaoCP.SetActive(false);
+			interagivelCaixaP.SetActive (false);
 			abrirPorta = true;
+		}
+		if (botaoCB.activeSelf & Input.GetKey (Mp.InteractKey) & PegueiLivro) {
+			botaoCB.SetActive(false);
+			interagivelCaixaB.SetActive (false);
 		}
 		if (abrirPorta) {
 			porta.transform.Translate (new Vector2 (0f, -0.1f));
@@ -120,11 +163,24 @@ public class Jogador : MonoBehaviour {
 		else if (coll.tag == "PassarCenaLivro") {
 			SceneManager.LoadScene ("SalaLivro");
 		}
+		else if(coll.tag == "CaixaO" & PegueiLivro){
+			botaoCO.SetActive (true);
+		}
+		else if(coll.tag == "CaixaP" & PegueiLivro){
+			botaoCP.SetActive (true);
+		}
+		else if(coll.tag == "CaixaB" & PegueiLivro){
+			botaoCB.SetActive (true);
+		}
 		else {
 			podePular = true;
 			botao.SetActive (false);
 			botaoSubir.SetActive (false);
 			botaoDescer.SetActive(false);
+			botaoCO.SetActive (false);
+			botaoCP.SetActive (false);
+			botaoCB.SetActive (false);
+
 		}
 	}
 
@@ -144,8 +200,14 @@ public class Jogador : MonoBehaviour {
 		else if (coll.tag == "Descer") {
 			botaoDescer.SetActive (true);
 		}
-		else if (coll.tag == "Porta" & abrirPorta == true & Input.GetKey(KeyCode.E)) {
-			Debug.Log ("Ganhou");
+		else if(coll.tag == "CaixaO" & PegueiLivro){
+			botaoCO.SetActive (true);
+		}
+		else if(coll.tag == "CaixaP" & PegueiLivro){
+			botaoCP.SetActive (true);
+		}
+		else if(coll.tag == "CaixaB" & PegueiLivro){
+			botaoCB.SetActive (true);
 		}
 		else {		
 			podePular = true;
@@ -164,6 +226,9 @@ public class Jogador : MonoBehaviour {
 			botao.SetActive (false);
 			botaoSubir.SetActive (false);
 			botaoDescer.SetActive (false);
+			botaoCO.SetActive (false);
+			botaoCP.SetActive (false);
+			botaoCB.SetActive (false);
 		}
 	}
 }
---
+++ Assets/Scripts/MovimentoPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoPlayer : MonoBehaviour {
+
+	public KeyCode jumpKey;
+	public KeyCode leftKey;
+	public KeyCode rightKey;
+	public KeyCode upKey;
+	public KeyCode downKey;
+	public KeyCode InteractKey;
+
+	public bool IsMovingLeft(){
+		return Input.GetKey (leftKey);
+	}
+	public bool IsMovingRight(){
+		return Input.GetKey (rightKey);
+	}
+	public bool IsJumping(){
+		return Input.GetKey (jumpKey);
+	}
+	public bool IsInteracting(){
+		return Input.GetKey (InteractKey);
+	}
+	public bool IsAscending(){
+		return Input.GetKey (upKey);
+	}
+	public bool IsDescending(){
+		return Input.GetKey (downKey);
+	}
+}
